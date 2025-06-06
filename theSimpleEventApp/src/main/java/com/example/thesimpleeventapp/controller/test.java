3d0b42c430f249b4c048f36cad63e1229793db92
Vule <vukasinpatkovic@gmail.com>
2025-06-06 14:27:06 +0200

    feat: added CORS handling

--- theSimpleEventApp/src/main/java/com/example/thesimpleeventapp/controller/test.java
+++ theSimpleEventApp/src/main/java/com/example/thesimpleeventapp/controller/test.java
@@ -1,13 +1,14 @@
 package com.example.thesimpleeventapp.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:5173")
 public class test {
-
 
     @GetMapping("/hello1234")
     public String hello() {
