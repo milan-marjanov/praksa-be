c4d508df508c41214cddf553e6c57148d6d28935
Stefan <milutinovic.stefan2003@gmail.com>
2025-06-23 14:29:31 +0200

    fix bug

--- theSimpleEventApp/src/main/java/com/example/thesimpleeventapp/dto/notification/NotificationDto.java
+++ theSimpleEventApp/src/main/java/com/example/thesimpleeventapp/dto/notification/NotificationDto.java
@@ -1,5 +1,6 @@
 package com.example.thesimpleeventapp.dto.notification;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,10 +13,17 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 public class NotificationDto {
+
     private Long id;
+
     private Long eventId;
+
     private String title;
+
     private String text;
+
     private LocalDateTime createdAt;
+
+    @JsonProperty("isRead")
     private boolean isRead;
 }
